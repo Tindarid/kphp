1b48cbf8698c412acbf6a13700f35c7ba091903c
Iskander Sharipov <i.sharipov@corp.vk.com>
2020-12-07 15:55:46 +0300

    enable exceptions inheritance
    * update CFG and other passes so they handle the program flow correctly;
      CatchesAll results in the can_throw=false,
      CatchesNone and CatchesSome mark a function with can_throw=true
    
    * codegen generates 2 new cases for the try/catch ops.
      If exception class has derived classes, is_a() is used;
      If exception class has no derived classes, hash comparison is used;
      When the code catches \Exception class, same codegen is used.
    
    * C$Exception becomes refcountable_polymorphic_php_classes_virt<>
      so it can be properly derived from;
      note that derived classes can also implement interfaces.
    
    Exceptions in CFG propagate like this: if try block catches
    all exceptions, we consider that nothing propagates.
    If try block doesn't catch \Exception, we consider all
    exceptions that can be created inside try block do propagate.
    
    Since existing KPHP code can only throw and catch \Exception,
    we get identical codegen and can_throw markers everywhere.

--- compiler/pipes/collect-main-edges.cpp
+++ compiler/pipes/collect-main-edges.cpp
@@ -320,12 +320,11 @@ void CollectMainEdgesPass::on_list(VertexAdaptor<op_list> list) {
 }
 
 void CollectMainEdgesPass::on_throw(VertexAdaptor<op_throw> throw_op) {
-  create_less(G->get_class("Exception"), throw_op->exception());
   create_less(throw_op->exception(), G->get_class("Exception"));
 }
 
 void CollectMainEdgesPass::on_try(VertexAdaptor<op_try> try_op) {
-  create_set(try_op->exception(), G->get_class("Exception"));
+  create_set(try_op->exception(), G->get_class(try_op->exception_type_declaration));
 }
 
 void CollectMainEdgesPass::on_set_op(VertexPtr v) {
